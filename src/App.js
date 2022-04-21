ba69f23bde1173049d31d4544183a0aff784c408
LapshinMikhail <mikhail.lapshin@initech.co.il>
2022-04-21 22:42:38 +0300

    second

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { sharedComponent } from "my-component";
 
 const App = () => {
   return (
       <div>
         THIS IS MY SHARED COMPONENT
+        <sharedComponent/>
       </div>
   );
 };
